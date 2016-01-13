2891dffca2f1e32452f3e7ab61b41a98b4f3820c
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-13 15:06:24 -0500

    weather data variable, and switching f to c function added

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var weatherData = {
+    city: document.querySelector("#city"),
+    weather: document.querySelector("#weather"),
+    temperature: document.querySelector("#temperature"),
+    temperatureValue: 0,
+    units: "F"
+};
