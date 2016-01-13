ccf6e45b0cb1d00665ac9ec9e59cc2cae4274155
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-13 15:16:56 -0500

    touching weather api

--- script/script.js
+++ script/script.js
@@ -3,5 +3,34 @@ var weatherData = {
     weather: document.querySelector("#weather"),
     temperature: document.querySelector("#temperature"),
     temperatureValue: 0,
-    units: "F"
+    units: "C"
 };
+
+function switchUnits(){
+  if (weatherData.units == "C"){
+    weatherData.temperatureValue = roundTemperature(weatherData.temperatureValue * 9/5 + 32);
+    weatherData.units = "F";
+  }
+  else{
+    weatherData.temperatureValue = roundTemperature((weatherData.temperatureValue -  32) * 5/9);
+    weatherData.units = "C";
+  }
+
+  weatherData.temperature.innerHTML = weatherData.temperatureValue + weatherData.units + ", ";      
+}
+
+function getLocationAndWeather(){
+    if(window.XMLHttpRequest){
+        var xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("load", function() {}, false);
+    xhr.addEventListener("error", function(){}, false);
+    
+    xhr.open("GET", "<URL>", true);
+    xhr.send();
+  }
+  else{
+    alert("Unable to fetch the location and weather data.");
+  }     
+}
+    
